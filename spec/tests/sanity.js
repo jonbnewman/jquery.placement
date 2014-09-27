e1a320687ff9ec10b1b12050d16b37dabe2e0f15
Jonathan Newman <jonathan@overfoc.us>
2014-09-27 09:33:53 -0500

    added one more unit test for basic functionality, hid test element in test runner

--- spec/tests/sanity.js
+++ spec/tests/sanity.js
@@ -1,7 +1,18 @@
 'use strict';
 
 describe('sanity', function () {
-  it('is present on jQuery.fn', function() {
-    expect(jQuery.fn.placement).to.be.a('function');
+  it('$.fn.placement() is present', function() {
+    expect( $.fn.placement ).to.be.a('function');
+  });
+
+  it('Check of test object shows correct left, right, top, bottom, width, height values', function() {
+    expect( $('.test-element').placement({ prop: [ 'width', 'height', 'left', 'right', 'top', 'bottom' ] }) ).to.eql({
+      width: 100,
+      height: 100,
+      left: 20,
+      right: 120,
+      top: 10,
+      bottom: 110
+    });
   });
 });
